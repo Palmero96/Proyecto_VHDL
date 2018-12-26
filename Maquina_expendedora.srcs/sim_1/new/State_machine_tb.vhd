8cdb7c7b81dff955fdc81886f58e9afecb495a7e
Palmero96 <38357897+Palmero96@users.noreply.github.com>
2018-12-26 23:43:37 +0100

    Testbench SS
    Elaborado y comprobado el funcionamiento de la máquina de estados mediante el testbench correspondiente.

--- Maquina_expendedora.srcs/sim_1/new/State_machine_tb.vhd
+++ Maquina_expendedora.srcs/sim_1/new/State_machine_tb.vhd
@@ -18,6 +18,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity State_machine_tb is
 end State_machine_tb;
@@ -40,12 +41,12 @@ architecture Behavioral of State_machine_tb is
     end component;
    
    --INPUT SIGNALS
-   signal order_number : STD_LOGIC_VECTOR(2 DOWNTO 0);
-   signal exact_amount : STD_LOGIC;
-   signal coin_error : STD_LOGIC;
-   signal overflow : STD_LOGIC;
+   signal order_number : STD_LOGIC_VECTOR(2 DOWNTO 0) := "000";
+   signal exact_amount : STD_LOGIC := '0';
+   signal coin_error : STD_LOGIC := '0';
+   signal overflow : STD_LOGIC := '0';
    signal rst : STD_LOGIC;
-   signal clk : STD_LOGIC;
+   signal clk : STD_LOGIC := '0';
    signal end_of_return : STD_LOGIC;
    signal end_of_output : STD_LOGIC;
    
@@ -54,8 +55,6 @@ architecture Behavioral of State_machine_tb is
    signal show : STD_LOGIC;
    signal output : STD_LOGIC;
    signal returns : STD_LOGIC;
-   
-   
 begin
 -------------------------------------
 --Declaramos el uut (Unit under test)
@@ -74,5 +73,59 @@ begin
         output => output,
         returns => returns 
     );
+    
+-----------------------------------------
+--Declaramos los diferentes pulsos del tb
+-----------------------------------------
+--Reloj
+    clk_tb: process(clk)
+    begin
+        clk <= not clk after 5 ns;
+    end process;
+--Reset   
+    rst_tb: process
+    begin
+        rst <= '0';
+        wait for 200 ns;
+        rst <= '1';
+        wait for 10 ns;
+    end process;
+--Order number
+    signals_tb: process
+    begin
+        wait for 50 ns;
+        order_number <= order_number + 1;
+        --if (order_number > 8) then
+        --    order_number <= "000";
+        --end if;
+    end process;
+--Exact amount, coin error & overflow
+    ea_tb: process(exact_amount, coin_error, overflow)
+    begin
+        exact_amount <= not exact_amount after 21 ns;
+        coin_error <= not coin_error after 26 ns;
+        overflow <= not overflow after 31 ns;
+    end process;
+--End of return & end of output
+    eo_tb: process
+    begin
+        end_of_return <= '0';
+        end_of_output <= '1';
+        wait for 55 ns;
+        end_of_return <= '1';
+        end_of_output <= '0';
+        wait for 10 ns;
+    end process;   
 
+----------------------------------------------------
+--Se comprueba si solo se ejecuta un estado a la vez
+----------------------------------------------------
+    ss_tb: process(standby, show, output, returns)
+    begin       
+        --assert(((standby AND show) OR (standby AND output) OR (standby AND returns) OR (show AND output)
+        --         OR (show AND returns) OR (output AND returns)) = '1')
+        --    report "Error: Estado no ejecutado correctamente"
+        --severity failure;
+    end process;
+        
 end Behavioral;
