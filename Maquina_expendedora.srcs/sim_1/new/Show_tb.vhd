f220825cb63cea703b31ef3b103e03f7556baa59
EmilioMiralles <39338208+EmilioMiralles@users.noreply.github.com>
2019-01-20 18:58:28 +0100

    Show y Show_tb
    He creado las fuentes show y show_tb.

--- Maquina_expendedora.srcs/sim_1/new/Show_tb.vhd
+++ Maquina_expendedora.srcs/sim_1/new/Show_tb.vhd
@@ -32,12 +32,92 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Show_tb is
---  Port ( );
 end Show_tb;
 
 architecture Behavioral of Show_tb is
-
+ component show PORT(
+       clk : in STD_LOGIC;
+       show_en : in STD_LOGIC;
+       dinero : in INTEGER;
+       s1: OUT std_logic_vector(7 downto 0);
+       s2: OUT std_logic_vector(7 downto 0);
+       s3: OUT std_logic_vector(7 downto 0);
+       s4: OUT std_logic_vector(7 downto 0);
+       s5: OUT std_logic_vector(7 downto 0);
+       s6: OUT std_logic_vector(7 downto 0);
+       s7: OUT std_logic_vector(7 downto 0);
+       s8: OUT std_logic_vector(7 downto 0)
+   );
+   end component;
+   
+      --Input signals
+      signal clk : STD_LOGIC := '0';
+      signal show_en : STD_LOGIC := '0';
+      signal dinero : INTEGER;
+      
+      --Output signals
+      signal s1 : STD_LOGIC_VECTOR(7 downto 0);
+      signal s2 : STD_LOGIC_VECTOR(7 downto 0);
+      signal s3 : STD_LOGIC_VECTOR(7 downto 0);
+      signal s4 : STD_LOGIC_VECTOR(7 downto 0);
+      signal s5 : STD_LOGIC_VECTOR(7 downto 0);
+      signal s6 : STD_LOGIC_VECTOR(7 downto 0);
+      signal s7 : STD_LOGIC_VECTOR(7 downto 0);
+      signal s8 : STD_LOGIC_VECTOR(7 downto 0);   
 begin
+---------------------------------------
+--Declaracion del unit under test (uut)
+---------------------------------------
+uut: show PORT MAP(
+    clk => clk,
+    show_en => show_en,
+    dinero => dinero,
+    s1 => s1,
+    s2 => s2,
+    s3 => s3,
+    s4 => s4,
+    s5 => s5,
+    s6 => s6,
+    s7 => s7,
+    s8 => s8
+);
 
+  ------------------
+    --Uso de reloj
+  ------------------
+   clk_tb: process(clk)
+   begin
+        clk <= not clk after 2 ns;
+        show_en <= not show_en after 5 ns;
+    end process;
+    
+  ------------------
+  --simulacion LETRAS
+  ------------------
+    s_tb: process 
+     begin
+         s1 <= s1;
+         s2 <= s2;
+         s3 <= s3;
+         s4 <= s4;
+         s5 <= s5;
+     end process;
+     
+   ------------------
+   --simulacion dinero
+   ------------------   
+     
+   dinero_tb: process(dinero,clk)
+        dinero = dinero + 5 after 7 ns;
+        s6 <= s6;
+        s7 <= s7;
+        s8 <= s8;
+         
+     end if;
+   end process;     
+    
+    
+   
+    
 
 end Behavioral;
--- Maquina_expendedora.srcs/sources_1/new/Show.vhd
+++ Maquina_expendedora.srcs/sources_1/new/Show.vhd
@@ -1,15 +1,15 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: UPM 
+-- Engineer: Emilio Miralles Rivera
 -- 
 -- Create Date: 31.12.2018 00:30:50
 -- Design Name: 
--- Module Name: Returns - Behavioral
--- Project Name: 
--- Target Devices: 
+-- Module Name: Show - Behavioral
+-- Project Name: Maquina Expendedora
+-- Target Devices: Nexys 4 DDR
 -- Tool Versions: 
 -- Description: 
--- 
+--             Entidad Show encargada de mostrar los numeros por el display de 7 segmentos
 -- Dependencies: 
 -- 
 -- Revision:
@@ -32,12 +32,109 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Show is
---  Port ( );
+  Port (
+        clk: IN std_logic;
+        show_en: IN std_logic;
+        dinero: IN integer;
+        
+        s1: OUT std_logic_vector(7 downto 0);
+        s2: OUT std_logic_vector(7 downto 0);
+        s3: OUT std_logic_vector(7 downto 0);
+        s4: OUT std_logic_vector(7 downto 0);
+        s5: OUT std_logic_vector(7 downto 0);
+        s6: OUT std_logic_vector(7 downto 0);
+        s7: OUT std_logic_vector(7 downto 0);
+        s8: OUT std_logic_vector(7 downto 0);
+        );
 end Show;
 
 architecture Behavioral of Show is
-
+    signal si1: STD_LOGIC := '0';
+    signal si2: STD_LOGIC := '0';
+    signal si3: STD_LOGIC := '0';
+    signal si4: STD_LOGIC := '0';
+    signal si5: STD_LOGIC := '0';
+    signal si6: STD_LOGIC := '0';
+    signal si7: STD_LOGIC := '0';
+    signal si8: STD_LOGIC := '0';
+    
 begin
 
+Conv_dinero: process (dinero)
+    if (dinero != 0) then 
+            with (dinero mod 100) select
+             s8 <= 
+                     "00110000" when 0,
+                     "00110001" when 1,
+                     "00110010" when 2,
+                     "00110011" when 3,
+                     "00110100" when 4,
+                     "00110101" when 5,
+                     "00110110" when 6,
+                     "00110111" when 7,
+                     "00111000" when 8,
+                     "00111001" when 9;
+        dinero <= dinero/100;
+             with (dinero mod 100) select
+             s7 <= 
+                     "00110000" when 0,
+                     "00110001" when 1,
+                     "00110010" when 2,
+                     "00110011" when 3,
+                     "00110100" when 4,
+                     "00110101" when 5,
+                     "00110110" when 6,
+                     "00110111" when 7,
+                     "00111000" when 8,
+                     "00111001" when 9;
+        dinero <= dinero/100;
+            with (dinero mod 100) select
+              s6 <= 
+                     "00110000" when 0,
+                     "00110001" when 1,
+                     "00110010" when 2,
+                     "00110011" when 3,
+                     "00110100" when 4,
+                     "00110101" when 5,
+                     "00110110" when 6,
+                     "00110111" when 7,
+                     "00111000" when 8,
+                     "00111001" when 9;
+        end if;    
+    end process;
+ 
+ Sincro_proc: process(clk)
+ if (clk'event and clk= '1') then
+    s1 <= si1;
+    s2 <= si2;
+    s3 <= si3;
+    s4 <= si4;
+    s5 <= si5;
+    s6 <= si6;
+    s7 <= si7;
+    s8 <= si8;
+    
+ end if;
+ 
+ END process;
+ 
+ Show_proc: process(show_en)
+  if (show = '1') then
+   s1 <= "01001101"  --M
+   s2 <= "01001111"  --O
+   s3 <= "01001110"  --N
+   s4 <= "01000101"  --E
+   s5 <= "01011001"  --Y
+  else 
+   s1 <= si1;
+   s2 <= si2;
+   s3 <= si3;
+   s4 <= si4;
+   s5 <= si5;
+   s6 <= si6;
+   s7 <= si7;
+   s8 <= si8;
+ end if;
+end process;
 
 end Behavioral;
