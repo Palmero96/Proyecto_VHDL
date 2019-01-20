7d0dc163c0d691f1885f3e8cd8c65be569699260
Emilio <emiliop.b@hotmal.com>
2019-01-20 20:40:29 +0100

    Output y su testibenchi

--- Maquina_expendedora.srcs/sim_1/new/Output_tb.vhd
+++ Maquina_expendedora.srcs/sim_1/new/Output_tb.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: upm
+-- Engineer: Emilio Peñaranda 
 -- 
 -- Create Date: 31.12.2018 00:34:21
 -- Design Name: 
@@ -24,7 +24,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.std_logic_unsigned.all;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -37,7 +38,69 @@ end Output_tb;
 
 architecture Behavioral of Output_tb is
 
+   component output PORT(
+                output_en: in std_logic;
+                Clk: in std_logic;
+                codbebida: in std_logic_vector (2 downto 0);
+                sacarbebida: out std_logic;
+                mens0: out std_logic_vector (7 downto 0);
+                mens1: out std_logic_vector (7 downto 0);
+                mens2: out std_logic_vector (7 downto 0);
+                mens3: out std_logic_vector (7 downto 0);
+                mens4: out std_logic_vector (7 downto 0);
+                mens5: out std_logic_vector (7 downto 0);
+                mens6: out std_logic_vector (7 downto 0);
+                mens7: out std_logic_vector (7 downto 0)
+   );
+   end component;
+
+
+ --Input signals
+   signal  output_en: std_logic:='0';
+   signal  Clk:  std_logic :='0';
+   signal codbebida: std_logic_vector(2 downto 0):=("000");
+   
+   --Output signals
+ signal  sacarbebida: std_logic := '0';
+ signal mens0:  std_logic_vector (7 downto 0);
+ signal mens1: std_logic_vector (7 downto 0);
+ signal mens2:  std_logic_vector (7 downto 0);
+ signal mens3:  std_logic_vector (7 downto 0);
+ signal mens4:  std_logic_vector (7 downto 0);
+ signal mens5:  std_logic_vector (7 downto 0);
+ signal mens6:  std_logic_vector (7 downto 0);
+ signal mens7:  std_logic_vector (7 downto 0);
+
 begin
 
+      uut : output PORT MAP(
+               output_en => output_en,
+               clk => clk,
+               sacarbebida => sacarbebida,
+               codbebida => codbebida,
+               mens0 => mens0,
+               mens1 => mens1,
+              mens2 => mens2,
+              mens3 => mens3,
+              mens4 => mens4,
+              mens5 => mens5,
+              mens6 => mens6,
+              mens7 => mens7
+               
+               );
+               
+      clk_tb: process(clk)
+                   begin
+                       clk <= not clk after 5 ns;
+                   end process;
+      sacarbebida_tb: process(output_en)
+                   begin
+                       output_en <= not output_en after 70 ns;
+                       end process;
+       codigobebida_tb: process
+                   begin
+                   codbebida <= codbebida + 1;
+                   wait for 16 ns;
+                   end process;
 
 end Behavioral;
--- Maquina_expendedora.srcs/sources_1/new/Output.vhd
+++ Maquina_expendedora.srcs/sources_1/new/Output.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: upm
+-- Engineer: Emilio Peñaranda
 -- 
 -- Create Date: 31.12.2018 00:30:50
 -- Design Name: 
@@ -32,12 +32,67 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Output is
---  Port ( );
+    Port ( 
+               output_en: in  std_logic;
+               Clk: in std_logic;
+               codbebida: in std_logic_vector (2 downto 0);
+               sacarbebida: out std_logic;
+               mens0: out std_logic_vector (7 downto 0);
+               mens1: out std_logic_vector (7 downto 0);
+               mens2: out std_logic_vector (7 downto 0);
+               mens3: out std_logic_vector (7 downto 0);
+               mens4: out std_logic_vector (7 downto 0);
+               mens5: out std_logic_vector (7 downto 0);
+               mens6: out std_logic_vector (7 downto 0);
+               mens7: out std_logic_vector (7 downto 0)
+    );
 end Output;
 
 architecture Behavioral of Output is
 
+
+
+
+
 begin
+
+
+asig_proc: process(codbebida, clk)
+    begin
+        if (rising_edge(clk)) then
+           mens0 <= "01010011";       --S
+           mens1 <= "01000001";       --A
+           mens2 <= "01001100";       --L
+           mens3 <= "01000101";       --E	
+           mens4 <= "01000000";        -- 
+           if (codbebida(0)= '0')then
+               mens5 <= "00000000" ;
+           else
+               mens5 <="00000001";
+               end if;    
+ 
+           if (codbebida(1)= '0')then
+               mens6 <= "00000000" ;
+           else
+               mens6 <="00000001";
+ 
+               end if; 
+           if (codbebida(2)= '0')then
+               mens7 <= "00000000" ;
+           else
+               mens7 <="00000001";
+           end if;                    
+        end if;
+    end process;
+
+sacar_proc: process(output_en)
+    begin
+        if (output_en='1') then
+           sacarbebida <= '1';
+        elsif(output_en ='0')then
+           sacarbebida <= '0';
+        end if;
+    end process;
 
 
 end Behavioral;
