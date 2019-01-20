48d8fba56de7dad336780296ca21f05a17681912
Palmero96 <38357897+Palmero96@users.noreply.github.com>
2019-01-20 20:40:24 +0100

    Solucion Errores
    Se ha solucionado errores de la ejecucion de la entidad
    
    Co-Authored-By: emiliomiralles <emiliomiralles@users.noreply.github.com>

--- Maquina_expendedora.srcs/sim_1/new/Show_tb.vhd
+++ Maquina_expendedora.srcs/sim_1/new/Show_tb.vhd
@@ -22,14 +22,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Show_tb is
 end Show_tb;
@@ -46,14 +38,15 @@ architecture Behavioral of Show_tb is
        s5: OUT std_logic_vector(7 downto 0);
        s6: OUT std_logic_vector(7 downto 0);
        s7: OUT std_logic_vector(7 downto 0);
-       s8: OUT std_logic_vector(7 downto 0)
+       s8: OUT std_logic_vector(7 downto 0);
+       trampita : out integer
    );
    end component;
    
       --Input signals
       signal clk : STD_LOGIC := '0';
       signal show_en : STD_LOGIC := '0';
-      signal dinero : INTEGER;
+      signal dinero : INTEGER := 0;
       
       --Output signals
       signal s1 : STD_LOGIC_VECTOR(7 downto 0);
@@ -63,61 +56,51 @@ architecture Behavioral of Show_tb is
       signal s5 : STD_LOGIC_VECTOR(7 downto 0);
       signal s6 : STD_LOGIC_VECTOR(7 downto 0);
       signal s7 : STD_LOGIC_VECTOR(7 downto 0);
-      signal s8 : STD_LOGIC_VECTOR(7 downto 0);   
+      signal s8 : STD_LOGIC_VECTOR(7 downto 0);  
+      
+      signal trampita : integer; 
 begin
----------------------------------------
---Declaracion del unit under test (uut)
----------------------------------------
-uut: show PORT MAP(
-    clk => clk,
-    show_en => show_en,
-    dinero => dinero,
-    s1 => s1,
-    s2 => s2,
-    s3 => s3,
-    s4 => s4,
-    s5 => s5,
-    s6 => s6,
-    s7 => s7,
-    s8 => s8
-);
+    ---------------------------------------
+    --Declaracion del unit under test (uut)
+    ---------------------------------------
+    uut: show PORT MAP(
+        clk => clk,
+        show_en => show_en,
+        dinero => dinero,
+        s1 => s1,
+        s2 => s2,
+        s3 => s3,
+        s4 => s4,
+        s5 => s5,
+        s6 => s6,
+        s7 => s7,
+        s8 => s8,
+        trampita => trampita
+    );
 
-  ------------------
-    --Uso de reloj
-  ------------------
-   clk_tb: process(clk)
-   begin
-        clk <= not clk after 2 ns;
-        show_en <= not show_en after 5 ns;
-    end process;
-    
-  ------------------
-  --simulacion LETRAS
-  ------------------
-    s_tb: process 
-     begin
-         s1 <= s1;
-         s2 <= s2;
-         s3 <= s3;
-         s4 <= s4;
-         s5 <= s5;
-     end process;
-     
-   ------------------
-   --simulacion dinero
-   ------------------   
-     
-   dinero_tb: process(dinero,clk)
-        dinero = dinero + 5 after 7 ns;
-        s6 <= s6;
-        s7 <= s7;
-        s8 <= s8;
+      ------------------
+        --Uso de reloj
+      ------------------
+       clk_tb: process(clk)
+       begin
+            clk <= not clk after 2 ns;
+        end process;
+        
+       show_tb : process
+       begin
+           show_en <= '0';
+           wait for 20 ns;
+           show_en <= '1';
+           wait for 1000 ns;
+       end process;
          
-     end if;
-   end process;     
-    
-    
-   
-    
-
+       ------------------
+       --simulacion dinero
+       ------------------   
+         
+       dinero_tb: process
+       begin
+            dinero <= (dinero + 5);
+            wait for 25 ns;
+       end process;     
 end Behavioral;
